8aa2bd9ae15b8762900def88253db009a194189f
Marco Aguilar <marcocaaguilar@gmail.com>
2019-02-25 00:03:50 -0600

    Se agrego la vista de registro

--- Index.php
+++ Index.php
@@ -1,5 +1,6 @@
+<?php include 'models/rutas.php'; ?>
 <!DOCTYPE html>
-<html lang="en">
+<html lang="es">
 
 <head>
 
@@ -12,12 +13,12 @@
   <title>La playita del brody</title>
 
   <!-- Custom fonts for this template-->
-  <link href="assets/vendor/fontawesome-free/css/all.min.css" rel="stylesheet" type="text/css">
+  <link href="<?php echo SERVERURL; ?>assets/vendor/fontawesome-free/css/all.min.css" rel="stylesheet" type="text/css">
   <link href="https://fonts.googleapis.com/css?family=Nunito:200,200i,300,300i,400,400i,600,600i,700,700i,800,800i,900,900i" rel="stylesheet">
 
   <!-- Custom styles for this template-->
-  <link href="assets/css/sb-admin-2.min.css" rel="stylesheet">
-  <link href="assets/css/styles.css" rel="stylesheet">
+  <link href="<?php echo SERVERURL; ?>assets/css/sb-admin-2.min.css" rel="stylesheet">
+  <link href="<?php echo SERVERURL; ?>assets/css/styles.css" rel="stylesheet">
 
 </head>
 
@@ -25,59 +26,30 @@
 
   <div class="container">
 
-    <!-- Outer Row -->
-    <div class="row justify-content-center">
-
-      <div class="col-xl-10 col-lg-12 col-md-9">
-
-        <div class="card o-hidden border-0 shadow-lg my-5">
-          <div class="card-body p-0">
-            <!-- Nested Row within Card Body -->
-            <div class="row">
-              <div class="col-lg-6 d-none d-lg-block bg-image">
-                
-              </div>
-              <div class="col-lg-6">
-                <div class="p-5">
-                  <div class="text-center">
-                    <h1 class="h4 text-gray-900 mb-4">Bienvenido de nuevo a la playita del brody!</h1>
-                  </div>
-                  <form class="user mt-5">
-                    <div class="form-group">
-                      <input type="email" class="form-control form-control-user" id="exampleInputEmail" aria-describedby="emailHelp" placeholder="Correo electronico...">
-                    </div>
-                    <div class="form-group">
-                      <input type="password" class="form-control form-control-user" id="exampleInputPassword" placeholder="Contraseña">
-                    </div>
-                    <a href="index.html" class="btn col-btn text-white btn-user btn-block mt-5">
-                      Iniciar sesión
-                    </a>
-                  </form>
-                  <hr>
-                  <div class="text-center mt-5">
-                    <a class="small" href="register.html">Crear una cuenta</a>
-                  </div>
-                </div>
-              </div>
-            </div>
-          </div>
-        </div>
-
-      </div>
-
-    </div>
+    <?php 
+      if (isset($_GET['view'])) {
+        $views = explode("/", $_GET['view']);
+        if (is_file('views/'.$views[0].'.php')) {
+          include 'views/'.$views[0].'.php';
+        } else {
+          include 'views/Index.php';
+        }
+      } else {
+        include 'views/Index.php';
+      }
+    ?>
 
   </div>
 
   <!-- Bootstrap core JavaScript-->
-  <script src="assets/vendor/jquery/jquery.min.js"></script>
-  <script src="assets/vendor/bootstrap/js/bootstrap.bundle.min.js"></script>
+  <script src="<?php echo SERVERURL; ?>assets/vendor/jquery/jquery.min.js"></script>
+  <script src="<?php echo SERVERURL; ?>assets/vendor/bootstrap/js/bootstrap.bundle.min.js"></script>
 
   <!-- Core plugin JavaScript-->
-  <script src="assets/vendor/jquery-easing/jquery.easing.min.js"></script>
+  <script src="<?php echo SERVERURL; ?>assets/vendor/jquery-easing/jquery.easing.min.js"></script>
 
   <!-- Custom scripts for all pages-->
-  <script src="assets/js/sb-admin-2.min.js"></script>
+  <script src="<?php echo SERVERURL; ?>assets/js/sb-admin-2.min.js"></script>
 
 </body>
 
---
+++ views/Index.php
@@ -0,0 +1,33 @@
+<div class="row justify-content-center">
+  <div class="col-xl-10 col-lg-12 col-md-9">
+    <div class="card o-hidden border-0 shadow-lg my-5">
+      <div class="card-body p-0">
+        <div class="row">
+          <div class="col-lg-6 d-none d-lg-block bg-image"></div>
+          <div class="col-lg-6">
+            <div class="p-5">
+              <div class="text-center">
+                <h1 class="h4 text-gray-900 mb-4">Bienvenido de nuevo a la playita del brody!</h1>
+              </div>
+              <form class="user mt-5">
+                <div class="form-group">
+                  <input type="email" class="form-control form-control-user" id="exampleInputEmail" aria-describedby="emailHelp" placeholder="Correo electronico...">
+                </div>
+                <div class="form-group">
+                  <input type="password" class="form-control form-control-user" id="exampleInputPassword" placeholder="Contraseña">
+                </div>
+                <a href="index.html" class="btn col-btn text-white btn-user btn-block mt-5">
+                  Iniciar sesión
+                </a>
+              </form>
+              <hr>
+              <div class="text-center mt-5">
+                <a class="small" href="<?php echo SERVERURL; ?>Register/">Crear una cuenta</a>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  </div>
+</div>
